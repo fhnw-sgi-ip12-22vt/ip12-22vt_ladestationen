2cde2b32b46115ee72a9504968c938205d4e0b59
AlePetSpi <65406057+AlePetSpi@users.noreply.github.com>
2022-12-02 13:01:15 +0100

    New project for javafx has been created.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.home.connectncharge {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.home.connectncharge to javafx.fxml;
+    exports com.home.connectncharge.controller;
+    opens com.home.connectncharge.controller to javafx.fxml;
+    exports com.home.connectncharge;
+    opens com.home.connectncharge.mvc to javafx.fxml;
+}
