88d6dd2f34475bb6ed10352e42ed98eff30b7d68
Gabugabu20 <92679773+Gabugabu20@users.noreply.github.com>
2023-04-07 14:36:29 +0200

    chore: Add to the software architecture two folders
    Description:
    - Add the assets folder which contains all the files that are needed outside of the software
    - Add the test folder with two default Controller Test files, the folder will contain all the necessary files to test the software

---
+++ src/test/java/ch/ladestation/connectcharge/controller/LoadingScreenControllerTest.java
@@ -0,0 +1,11 @@
+package ch.ladestation.connectcharge.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LoadingScreenControllerTest {
+
+}
