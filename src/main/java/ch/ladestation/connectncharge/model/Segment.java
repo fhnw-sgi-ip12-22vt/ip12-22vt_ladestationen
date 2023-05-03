f5bfec4f3a21ee9fba32f507a8a04f2eb14ec5ef
MMMMMNG <113881019+MMMMMNG@users.noreply.github.com>
2023-05-03 16:56:10 +0200

    chore: refactor Segment class
    this class is now part of the model and much of its functionality was transferred away from it as a result (transferred to PUIController).

--- src/main/java/ch/ladestation/connectncharge/model/Segment.java
+++ src/main/java/ch/ladestation/connectncharge/model/Segment.java
@@ -1,221 +1,74 @@
 package ch.ladestation.connectncharge.model;
 
 import com.github.mbelling.ws281x.Color;
-import com.github.mbelling.ws281x.LedStrip;
-import com.pi4j.util.Console;
 
 import ch.ladestation.connectncharge.pui.Component;
-import ch.ladestation.connectncharge.pui.MCP23S17;
-
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public abstract class Segment extends Component {
 
     public static final String HOUSE_FLAG = "H";
 
-    private final int index;
-
     /**
-     * The ledstrip this edge is part of.
+     * the segment index according to LEDSegments.csv
      */
-    private final LedStrip strip;
+    private final int segmentIndex;
+
+
     /**
      * The start pixel of this edge
      */
     private final int startIndex;
+
     /**
      * The end pixel of this edge
      */
     private final int endIndex;
     /**
-     * how many milliseconds to cooldown after toggle
-     */
-    private final int cOOLDOWNMS = 100;
-    /**
-     * prevent multiple threads writing cooldown at the same time
-     */
-    private final Object cooldownWriteLock = new Object();
-    /**
-     * the callback if this edge changed state
-     */
-    private SegmentStateChange change;
-    /**
      * The color this segment should display
      */
     private Color color = Color.GREEN;
     /**
-     * Cost, if this segment is an edge, 0 otherwise
-     */
-    private int cost;
-    /**
      * Whether this edge is shining
      */
     private boolean isOn = false;
-    /**
-     * whether the edge has toggled recently and further interrupts will be ignored
-     */
-    private boolean inCooldown = false;
 
     /**
      * Basic constructor for the {@code Edge} class
      *
-     * @param index
-     * @param strip      the strip of which this edge is a part.
-     * @param startIndex the start pixel of the edge.
-     * @param endIndex   the end pixel of the edge
-     */
-    public Segment(int index, LedStrip strip, int startIndex, int endIndex) {
-        this.index = index;
-        this.strip = strip;
-        this.startIndex = startIndex;
-        this.endIndex = endIndex;
-    }
-
-    /**
-     * Constructor for the {@code Edge} class that expects one or multiple interrupt enabled input pins
-     * that can be used to toggle the edge.
-     *
-     * @param index
-     * @param strip         the strip of which this edge is a part.
-     * @param interruptPins the pin(s) on which to attach toggle on interrupt
-     * @param startIndex    the start pixel of the edge.
-     * @param endIndex      the end pixel of the edge
-     * @param cost          the cost of this particular segment (if applicable. If this is a house segment, it's N/A)
-     */
-    public Segment(int index, LedStrip strip, MCP23S17.PinView[] interruptPins, 
-            int startIndex, int endIndex, int cost) {
-        this.index = index;
-        addInterruptPins(interruptPins);
-        this.strip = strip;
-        this.startIndex = startIndex;
-        this.endIndex = endIndex;
-        this.cost = cost;
-    }
-
-    /**
-     * same constructor as the previous one but with only one interrupt pin.
-     *
-     * @param index
-     * @param strip        the strip of which this edge is a part.
-     * @param interruptPin the pin on which to attach toggle on interrupt
+     * @param segmentIndex
      * @param startIndex   the start pixel of the edge.
-     * @param cost         the cost if this segment represents an edge
-     * @param callback     the callback to update total cost.
      * @param endIndex     the end pixel of the edge
+     * @param color        the color this segment will be visualized with
      */
-    public Segment(int index, LedStrip strip, MCP23S17.PinView interruptPin, int startIndex, int endIndex, int cost,
-                   SegmentStateChange callback) {
-        this(index, strip, new MCP23S17.PinView[] {interruptPin}, startIndex, endIndex, cost);
-        this.change = callback;
-    }
-
-    /**
-     * same constructor as the basic constructor, but with a color for the segment
-     *
-     * @param index
-     * @param strip      the strip of which this edge is a part.
-     * @param color      the segment's color of type {@link Color}
-     * @param startIndex the start pixel of the edge.
-     * @param endIndex   the end pixel of the edge
-     */
-    public Segment(int index, LedStrip strip, Color color, int startIndex, int endIndex) {
-        this(index, strip, startIndex, endIndex);
+    public Segment(int segmentIndex, int startIndex, int endIndex, Color color) {
+        this.segmentIndex = segmentIndex;
+        this.startIndex = startIndex;
+        this.endIndex = endIndex;
         this.color = color;
     }
 
-    public static List<Segment> createSegemntsAccordingToCSV(Console console, LedStrip strip,
-                                                             ArrayList<ArrayList<MCP23S17.PinView>> pins,
-                                                             InputStream file, SegmentStateChange changeCallBack) {
-        var semicolonDelimiter = ";";
 
-        List<List<String>> records = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(file, StandardCharsets.UTF_8))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] values = line.split(semicolonDelimiter);
-                records.add(Arrays.asList(values));
-            }
-        } catch (IOException e) {
-            console.println("Exception when reading CSV: " + e.getMessage());
-        }
-
-        int runningTotal = 0;
-        var retSegments = new ArrayList<Segment>();
-        for (int i = 1; i < records.size(); i++) {
-            var record = records.get(i);
-            int startIndex = runningTotal;
-            runningTotal += Integer.parseInt(record.get(1));
-            int endIndex = runningTotal - 1;
-
-            Segment segment;
-            if (record.get(2).equals(HOUSE_FLAG)) {
-                segment = new Node(Integer.parseInt(record.get(0)), strip, Color.BLUE, startIndex, endIndex);
-            } else {
-                int chip = Integer.parseInt(record.get(2));
-                int pin = Integer.parseInt(record.get(3));
-                int cost = Integer.parseInt(record.get(4));
-                segment = new Edge(Integer.parseInt(record.get(0)), strip, pins.get(chip).get(pin), startIndex, 
-                endIndex, cost, changeCallBack);
-            }
-            retSegments.add(segment);
-        }
-        return retSegments;
+    public int getStartIndex() {
+        return startIndex;
     }
 
-    /**
-     * Adds one or more {@link MCP23S17.PinView} objects that are expected to be
-     * interrupt-enabled, pulled-up input pins.
-     * The method will attach state-change listeners to those pins that toggle the edge.
-     *
-     * @param interruptPins the {@link MCP23S17.PinView} objects to which state-change listeners will be attached
-     */
-    private void addInterruptPins(MCP23S17.PinView[] interruptPins) {
-        for (MCP23S17.PinView interruptPin : interruptPins) {
-            interruptPin.addListener((boolean capturedValue, MCP23S17.Pin pin) -> {
-                if (!capturedValue) {
-                    return;
-                }
-                toggle();
-                logInfo("toggled pin " + pin.name());
-            });
-        }
-    }
-
-    /**
-     * Toggle this edge's state. Synchronized on strip for thread safety.
-     */
-    public void toggle() {
-        synchronized (strip) {
-            if (isOn) {
-                for (int i = startIndex; i <= endIndex; ++i) {
-                    strip.setPixel(i, 0, 0, 0);
-                }
-            } else {
-                for (int i = startIndex; i <= endIndex; ++i) {
-                    strip.setPixel(i, color);
-                }
-            }
-            isOn = !isOn;
-
-            if (this.change != null) {
-                int cost = (isOn ? 1 : -1) * this.cost;
-                this.change.onStateChange(cost);
-            }
-            
-            strip.render();
-        }
+    public int getEndIndex() {
+        return endIndex;
     }
 
     public void log(String msg) {
         logInfo(msg);
     }
 
+    public Color getColor() {
+        return color;
+    }
+
+    public boolean isOn() {
+        return isOn;
+    }
+
     public interface SegmentStateChange {
         void onStateChange(int deltaCost);
     }
-
 }
