7768a4c3e1034e8e627db1bb6633a10c90b27620
PsynoxCH <113668452+PsynoxCH@users.noreply.ggitithub.com>
2023-05-10 14:24:07 +0200

    feat: Back Button is created with Code. Design of the Buttons has Changed. Scene-Change is working.

--- src/main/java/ch/ladestation/connectncharge/controller/AdminHomeController.java
+++ src/main/java/ch/ladestation/connectncharge/controller/AdminHomeController.java
@@ -1,5 +1,22 @@
 package ch.ladestation.connectncharge.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
 public class AdminHomeController {
 
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private void handleBackButton(ActionEvent event) throws IOException {
+        StageHandler.openStage("/ch/ladestation/connectncharge/homepage.fxml", "/css/style.css",
+            (Stage) ((Node) event.getSource()).getScene().getWindow());
+    }
 }
