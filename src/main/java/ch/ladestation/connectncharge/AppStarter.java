9146aa66d53f48f7a30a8fee95253ba1dd2bdbb8
Gabugabu20 <92679773+Gabugabu20@users.noreply.github.com>
2023-05-11 15:05:05 +0200

    fix: Fixed Checkstyle violations Description: - Removed Unused Imports - Moved levels to textfiles directory

--- src/main/java/ch/ladestation/connectncharge/AppStarter.java
+++ src/main/java/ch/ladestation/connectncharge/AppStarter.java
@@ -3,7 +3,10 @@ package ch.ladestation.connectncharge;
 import ch.ladestation.connectncharge.controller.ApplicationController;
 import ch.ladestation.connectncharge.controller.StageHandler;
 import ch.ladestation.connectncharge.model.Game;
+import ch.ladestation.connectncharge.pui.GamePUI;
+import ch.ladestation.connectncharge.util.Pi4JContext;
 import ch.ladestation.connectncharge.util.mvcbase.MvcLogger;
+import com.pi4j.context.Context;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -14,27 +17,19 @@ public class AppStarter extends Application {
     private static ApplicationController controller;
 
     public static void main(String[] args) {
-        /*Context pi4J = Pi4JContext.createContext();
+        Context pi4J = Pi4JContext.createContext();
 
         controller = new ApplicationController(new Game());
         var gPUI = new GamePUI(controller, pi4J);
 
         LOGGER.logInfo("App started");
 
-        /*int[] terms = {81, 27, 11, 31, 52, 47, 33, 62, 77, 16, 95, 18, 67};
-
-        var terminalNodes = Arrays.stream(terms)
-            .mapToObj(gPUI::lookUpSegmentIdToSegment)
-            .map(seg -> (Node) seg)
-            .toArray(Node[]::new);
-        controller.setTerminals(terminalNodes);*/
-
         controller.setGPUI(gPUI);
         controller.loadLevels();
         controller.loadNextLevel();
 
         LOGGER.logInfo("App started");
-        
+
         // This will ensure Pi4J is properly finished. All I/O instances are
         // released by the system and shutdown in the appropriate
         // manner. It will also ensure that any background
@@ -44,7 +39,7 @@ public class AppStarter extends Application {
             controller.shutdown();
             pi4J.shutdown();
             LOGGER.logInfo("App stopped");
-        }));*/
+        }));
         launch();
     }
 
