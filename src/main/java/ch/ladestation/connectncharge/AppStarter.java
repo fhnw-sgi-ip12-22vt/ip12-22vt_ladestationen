097f5d0fcc0b3c0b79b8aae1b7b6fd7de9e6b53c
SheniaScherer <69905007+SheniaScherer@users.noreply.github.com>
2023-05-11 15:59:28 +0200

    fix: appstarter deleted some comments

--- src/main/java/ch/ladestation/connectncharge/AppStarter.java
+++ src/main/java/ch/ladestation/connectncharge/AppStarter.java
@@ -3,7 +3,10 @@ package ch.ladestation.connectncharge;
 import ch.ladestation.connectncharge.controller.ApplicationController;
 import ch.ladestation.connectncharge.controller.StageHandler;
 import ch.ladestation.connectncharge.model.Game;
+import ch.ladestation.connectncharge.pui.GamePUI;
+import ch.ladestation.connectncharge.util.Pi4JContext;
 import ch.ladestation.connectncharge.util.mvcbase.MvcLogger;
+import com.pi4j.context.Context;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -14,7 +17,7 @@ public class AppStarter extends Application {
     private static ApplicationController controller;
 
     public static void main(String[] args) {
-        /*Context pi4J = Pi4JContext.createContext();
+        Context pi4J = Pi4JContext.createContext();
 
         controller = new ApplicationController(new Game());
         var gPUI = new GamePUI(controller, pi4J);
@@ -33,15 +36,15 @@ public class AppStarter extends Application {
             controller.shutdown();
             pi4J.shutdown();
             LOGGER.logInfo("App stopped");
-        }));*/
+        }));
         launch();
     }
 
     @Override
     public void start(Stage stage) throws IOException {
         StageHandler.setStage(stage);
-        //StageHandler.setController(controller);
-        StageHandler.setController(new ApplicationController(new Game()));
+        StageHandler.setController(controller);
+        //StageHandler.setController(new ApplicationController(new Game()));
         StageHandler.openStage("/ch/ladestation/connectncharge/gamepage.fxml", "/css/style.css");
     }
 }
