8a6536e5ee642be6a3377d6df21789fb6fbc2892
Gabugabu20 <92679773+Gabugabu20@users.noreply.github.com>
2023-05-11 16:09:15 +0200

    fix: Fixed Level 4 Description: - level 4 has now a correct solution

--- src/main/java/ch/ladestation/connectncharge/AppStarter.java
+++ src/main/java/ch/ladestation/connectncharge/AppStarter.java
@@ -46,8 +46,8 @@ public class AppStarter extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         StageHandler.setStage(stage);
-        //StageHandler.setController(controller);
-        StageHandler.setController(new ApplicationController(new Game()));
+        StageHandler.setController(controller);
+        //StageHandler.setController(new ApplicationController(new Game()));
         StageHandler.openStage("/ch/ladestation/connectncharge/gamepage.fxml", "/css/style.css");
     }
 }
